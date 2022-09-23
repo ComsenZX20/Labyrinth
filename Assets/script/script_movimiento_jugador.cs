86ab5be31b6a6ec2cbe5779d523a207845883df6
ComsenZX2O <>
2022-09-23 13:52:26 +0100

    Second commit

--- Assets/script/script_movimiento_jugador.cs
+++ Assets/script/script_movimiento_jugador.cs
@@ -7,7 +7,7 @@ public class script_movimiento_jugador : MonoBehaviour
    public float MovimientoEjeX;
    public float MovimientoEjeY;
     public float MovimientoEjeZ;
-    public float VelocidadMovimiento = 10.5f;
+    public float VelocidadMovimiento = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +19,10 @@ public class script_movimiento_jugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovimientoEjeZ = Input.GetAxis("Horizontal") * Time.deltaTime * VelocidadMovimiento;
-        MovimientoEjeX = -Input.GetAxis("Vertical") * Time.deltaTime * VelocidadMovimiento;
-       
+        MovimientoEjeX = Input.GetAxis("Vertical") * Time.deltaTime * VelocidadMovimiento;
+
+        MovimientoEjeZ = -Input.GetAxis("Horizontal") * Time.deltaTime * VelocidadMovimiento;
+
         transform.Translate(MovimientoEjeX, MovimientoEjeY, MovimientoEjeZ);  
     }
 }
