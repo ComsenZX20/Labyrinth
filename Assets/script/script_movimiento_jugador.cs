a0add829e8c3484ea94577ff60750c14041226cc
ComsenZX2O <>
2022-09-23 11:47:09 +0100

    Primer Commit

---
+++ Assets/script/script_movimiento_jugador.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_movimiento_jugador : MonoBehaviour
+{
+   public float MovimientoEjeX;
+   public float MovimientoEjeY;
+    public float MovimientoEjeZ;
+    public float VelocidadMovimiento = 10.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimientoEjeZ = Input.GetAxis("Horizontal") * Time.deltaTime * VelocidadMovimiento;
+        MovimientoEjeX = -Input.GetAxis("Vertical") * Time.deltaTime * VelocidadMovimiento;
+       
+        transform.Translate(MovimientoEjeX, MovimientoEjeY, MovimientoEjeZ);  
+    }
+}
